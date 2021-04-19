cec54c89640748485c61351304ba4d641b3ac20c
Riya <riya@2202.com>
2021-04-20 01:28:55 +0530

    Bokks/Create/Index/New

--- server.js
+++ server.js
@@ -5,23 +5,47 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
 
 app.set('view engine','ejs');
 app.set('views',__dirname + '/views')
 app.set('layout','layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit : '10mb', extended: false}))
+
 
 const mongoose = require('mongoose')
+/*
+-------------------------------------------
+Don't Use it..!!!!
+-------------------------------------------
 mongoose.connect(process.env.DATABASE_URL,
     {useNewUrlParser : true},
     { useUnifiedTopology: true } )
 const db = mongoose.connection
 db.on('error',error => console.error(error)) 
 db.once('open',() => console.log('connected to mongoose')) 
+*/
+
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+ }).then(()=>{
+     console.log(`connection to database established`)
+ }).catch(err=>{
+     console.log(`db error ${err.message}`);
+     process.exit(-1)
+ })
 
 app.use('/',indexRouter)
+app.use('/authors',authorRouter)
+app.use('/books',bookRouter)
 
 app.listen(process.env.PORT || 3000)
